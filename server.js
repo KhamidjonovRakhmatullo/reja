4761da1faac9daf11f3daad66dd26ed7b9e9e5f6
Rakhmatullo <richman.uzb1676@mail.ru>
2025-12-24 16:49:52 +0900

    remove notes and clean comments

--- server.js
+++ server.js
@@ -1,73 +1,34 @@
-/**
- * Backend Server Side Rendering (BSSR)
- */
-
-console.log("Backend Server Side Rendering (BSSR) started");
-
-// ===============================
-// Imports
-// ===============================
+console.log("Web server run!");
 const express = require("express");
 const http = require("http");
 
-// ===============================
-// App Initialization
-// ===============================
 const app = express();
 
-// ===============================
-// 1. Request Entrance
-// ===============================
-
-// * Let coming browser request to enter public folder (CSS, images, JS)
+// 1. Middleware (Request Processing)
 app.use(express.static("public"));
-// * Convert incoming json data to object structure
 app.use(express.json());
-// * Get POSTed input data which coming from traditional form request
 app.use(express.urlencoded({extended: true}));
 
-
-// ===============================
 // 2. Session
-// ===============================
 
-
-// ===============================
-// 3. View Engine (EJS)
-// ===============================
-
-// Direction to the folder
+// 3. View Engine
 app.set("views", "views");
-// Set EJS as the template engine
 app.set("view engine", "ejs");
 
 
-// ===============================
-// 4. Routes
-// ===============================
-
-//post method
+// 4. Routes (Request Handlers)
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({result: "Success! You have add a new item"});
 });
 
-//get method
 app.get("/", function(req, res) {
     res.render("harid");
 });
 
 
-// ---- ---- ---- ----
-//Server Configuration
-
-// Create HTTP server
 const server = http.createServer(app);
-
-// PORT 
 let PORT = 3000;
-
-// Start server
 server.listen(PORT, function() {
     console.log(`Server is running on port: ${PORT}`);
 });
