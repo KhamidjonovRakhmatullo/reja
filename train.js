8c36f5abb1ecb98a5d3c823fab32790f25a675df
Rakhmatullo <richman.uzb1676@mail.ru>
2026-01-01 18:50:31 +0900

    fix: refactoring

--- train.js
+++ train.js
@@ -10,19 +10,23 @@ hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga soni
 MASALAN countLetter("e", "engineer") 3ni return qiladi.
  */
 
-function countLetter(letter, word){
-    let count = 0;
+// function countLetter(letter, word){
+//     let count = 0;
 
-    for(let i = 0; i < word.length; i++){
-        if(word[i] === letter){
-            count++;
-        }
-    }
-    return count;
-}
+//     for(let i = 0; i < word.length; i++){
+//         let currentChar = word[i];
+//         console.log("curr:", currentChar);
 
-const result = countLetter("a", "alan");
-console.log(result);
+//         if(currentChar === letter){
+//             count++
+//         };
+//     };
+
+//     return count
+// }
+
+// const result = countLetter("a", "alan");
+// console.log(result);
 
 
 
@@ -38,14 +42,14 @@ console.log(result);
 // console.log("Jack Ma maslahatlari!");
 
 // data
-const list = [
-    "1. Yaxshi talaba boling!", //0-20
-    "2. Togri boshliq tanlang va koproq hato qiling!", // 20-30
-    "3. O'zingizga ishlashni boshlang!", // 30-40
-    "4. Siz kuchli bo'lgan narsalarni qiling!", // 40-50
-    "5. Yoshlarga invertitsiya qiling!", // 50-60
-    "6. Endi dam oling, foydasi yo'q!" //60+
-];
+// const list = [
+//     "1. Yaxshi talaba boling!", //0-20
+//     "2. Togri boshliq tanlang va koproq hato qiling!", // 20-30
+//     "3. O'zingizga ishlashni boshlang!", // 30-40
+//     "4. Siz kuchli bo'lgan narsalarni qiling!", // 40-50
+//     "5. Yoshlarga invertitsiya qiling!", // 50-60
+//     "6. Endi dam oling, foydasi yo'q!" //60+
+// ];
 
 
 
@@ -54,28 +58,28 @@ const list = [
 // ======================================= Callback function ===============================================
 
 // define callback func
-function maslahatBering(yosh, callback){
-    if(typeof yosh !== 'number') callback("Please, insert number!", null);
-    else if(yosh <= 20) callback(null, list[0]);
-    else if(yosh > 20 && yosh <= 30) callback(null, list[1]);
-    else if(yosh > 30 && yosh <= 40) callback(null, list[2]);
-    else if(yosh > 40 && yosh <= 50) callback(null, list[3]);
-    else if(yosh > 50 && yosh <= 60) callback(null, list[4]);
-    else{
-        // ex 1
-        callback(null, list[5]);
+// function maslahatBering(yosh, callback){
+//     if(typeof yosh !== 'number') callback("Please, insert number!", null);
+//     else if(yosh <= 20) callback(null, list[0]);
+//     else if(yosh > 20 && yosh <= 30) callback(null, list[1]);
+//     else if(yosh > 30 && yosh <= 40) callback(null, list[2]);
+//     else if(yosh > 40 && yosh <= 50) callback(null, list[3]);
+//     else if(yosh > 50 && yosh <= 60) callback(null, list[4]);
+//     else{
+//         // ex 1
+//         callback(null, list[5]);
 
-        // ex 2
-        // setTimeout(function(){
-        //     callback(null, list[5]);
-        // }, 3000);
+//         // ex 2
+//         // setTimeout(function(){
+//         //     callback(null, list[5]);
+//         // }, 3000);
 
-        // ex 3
-        // setInterval(function(){
-        //  callback(null, list[5])
-        // }, 1000);
-    };
-};
+//         // ex 3
+//         // setInterval(function(){
+//         //  callback(null, list[5])
+//         // }, 1000);
+//     };
+// };
 
 // call func
 // console.log("passed here: 0");
@@ -104,25 +108,25 @@ function maslahatBering(yosh, callback){
 //async functions get response after all sync functions got their responses.
 
 // define async func
-async function maslahatBering2(yosh){
-    if(typeof yosh !== 'number') throw new Error("Please, insert number!");
-    else if(yosh <= 20) return list[0];
-    else if(yosh > 20 && yosh <=30) return list[1];
-    else if(yosh > 30 && yosh <=40) return list[2];
-    else if(yosh > 40 && yosh <=50) return list[3];
-    else if(yosh > 50 && yosh <=60) return list[4];
-    else{
-        // ex 1
-        return list[5];
+// async function maslahatBering2(yosh){
+//     if(typeof yosh !== 'number') throw new Error("Please, insert number!");
+//     else if(yosh <= 20) return list[0];
+//     else if(yosh > 20 && yosh <=30) return list[1];
+//     else if(yosh > 30 && yosh <=40) return list[2];
+//     else if(yosh > 40 && yosh <=50) return list[3];
+//     else if(yosh > 50 && yosh <=60) return list[4];
+//     else{
+//         // ex 1
+//         return list[5];
 
-        // ex 2  "possible to use core func in async with Promise func"
-        // return new Promise((resolve, reject) => {
-        //   setTimeout(() => {
-        //     resolve(list[5]);
-        //   }, 3000);
-        // });
-    };
-};
+//         // ex 2  "possible to use core func in async with Promise func"
+//         // return new Promise((resolve, reject) => {
+//         //   setTimeout(() => {
+//         //     resolve(list[5]);
+//         //   }, 3000);
+//         // });
+//     };
+// };
 
 //------ * call via then / catch * ------//
 
